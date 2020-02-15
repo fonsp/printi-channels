b49d2645ea0aeb1100ea70d1d5ded6b303266804
Fons van der Plas <fonsvdplas@gmail.com>
2020-02-15 02:44:31 +0100

    ✂ Rewrote trim with PIL; 8-bit image depth

--- trim.py
+++ trim.py
@@ -1,23 +1,12 @@
-import os
-import skimage.io
-import numpy as np
-
+from PIL import Image, ImageOps
 
 def trim_file(path):
-    original = skimage.io.imread(path)
-    trimmed = trim(original)
-    if trimmed is None:
-        return 'all white'
-    skimage.io.imsave(path, trimmed)
+    im = Image.open(path).convert("L")
+    # We invert the image, and ask PIL for the bounding box around all non-black pixel in the image.
+    bbox = ImageOps.invert(im).getbbox()
 
+    if bbox is None:
+        return "all white"
 
-def trim(image):
-    white_mask = np.equal(image, [255, 255, 255, 255])
-    white_pixels = np.logical_and.reduce(white_mask, -1)
-    white_rows = np.logical_and.reduce(white_pixels, -1)
-    nonwhite_indices = np.logical_not(white_rows).nonzero()[0]
-    if len(nonwhite_indices) == 0:
-        return None
-    last_included = nonwhite_indices[-1]
-    return image[: last_included + 1, :, :]
-
+    im_cropped = im.crop((0, 0, im.width, bbox[3]))
+    im_cropped.save(path)
